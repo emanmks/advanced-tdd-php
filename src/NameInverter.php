3d5fde34d396b2b176e001d4dc4018daff2248b9
Solaiman Mansyur <solaiman.mansyur@gmail.com>
2019-03-12 12:14:49 +0800

    given null and given empty tests

---
+++ src/NameInverter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class NameInverter {
+
+    public function invert(string $name): string
+    {
+        return "";
+    }
+}
---
+++ tests/NameInverterTest.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use App\NameInverter;
+
+class NameInverterTest extends TestCase
+{
+    public function testGivenNullThrowsAnException(): void
+    {
+        $this->expectException(TypeError::class);
+
+        $inverter = new NameInverter();
+        $inverter->invert(null);
+    }
+
+    public function testGivenEmptyReturnEmptyString(): void
+    {
+        $inverter = new NameInverter();
+        $this->assertEquals("", $inverter->invert(""));
+    }
+}
